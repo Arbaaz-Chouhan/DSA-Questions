08847fe7be530fb8acab60656c93b941cc2a8231
Arbaaz chouhan <chouhanarbaaz4@gmail.com>
2024-12-26 23:11:22 +0530

    DSA-Questions

--- JS/Basic-Hashing/count-frequency.js
+++ JS/Basic-Hashing/count-frequency.js
@@ -1,16 +1,15 @@
 // Count frequency of each element in the array
 // Problem statement: Given an array, we have found the number of occurrences of each element in the array.
-  
+
 function CountFrequency(arr) {
-    const frequencyMap = {};
-    for (let num of arr) {
-      frequencyMap[num] = (frequencyMap[num] || 0) + 1;
-    }
-  
-    return frequencyMap;
+  const frequencyMap = {};
+  for (let num of arr) {
+    frequencyMap[num] = (frequencyMap[num] || 0) + 1;
   }
-  
-  
+
+  return frequencyMap;
+}
+
+
 //   console.log(CountFrequency([1, 2, 1, 3, 2])); // Output: { '1': 2, '2': 2, '3': 1 }
 //   console.log(CountFrequency([1, 2, 3, 1, 2, 2, 3, 1, 2, 4, 2, 5, 3, 6, 8])); // Output: { '1': 3, '2': 5, '3': 3, '4': 1, '5': 1, '6': 1, '8': 1 }
-  
