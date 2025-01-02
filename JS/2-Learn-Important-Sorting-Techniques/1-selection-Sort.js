b40a679390b4d4a71166b3ed803c5e3ecb962e67
Arbaaz chouhan <chouhanarbaaz4@gmail.com>
2025-01-02 23:08:56 +0530

    Solve-Daily-DSA-Question

---
+++ JS/2-Learn-Important-Sorting-Techniques/1-selection-Sort.js
@@ -0,0 +1,19 @@
+// Selection Sort Algorithm
+// Problem Statement: Given an array of N integers, write a program to implement the Selection sorting algorithm.
+
+
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i; j < arr.length; j++) {
+
+            if (arr[j] < arr[i]) {
+                [arr[i], arr[j]] = [arr[j], arr[i]];
+            }
+        }
+    }
+    return arr;
+}
+
+let array = [13, 46, 24, 52, 20, 9];
+console.log(selectionSort(array));
+
