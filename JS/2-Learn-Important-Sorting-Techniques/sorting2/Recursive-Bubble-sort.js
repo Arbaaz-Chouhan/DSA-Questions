9376b16ccf2737efce1146eda96b1d21e1baad4f
Arbaaz chouhan <chouhanarbaaz4@gmail.com>
2025-01-12 01:04:51 +0530

    Daily-Solve-DSA-Question

--- JS/2-Learn-Important-Sorting-Techniques/sorting2/Recursive-Bubble-sort.js
+++ JS/2-Learn-Important-Sorting-Techniques/sorting2/Recursive-Bubble-sort.js
@@ -1,16 +1,15 @@
 function RecursiveBubbleSort(arr, n = arr.length) {
-    
     if (n === 1) {
         return arr;
     }
-
-    for (let i = 0; i < n - 1; i++) {
+     for (let i = 0; i < n - 1; i++) {
         if (arr[i] > arr[i + 1]) {
             [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
         }
     }
-
     return RecursiveBubbleSort(arr, n - 1);
 }
+
 const result = RecursiveBubbleSort([2, 3, 1, 4, 6, 5, 7]);
 console.log("Sorted Array:", result);
+
