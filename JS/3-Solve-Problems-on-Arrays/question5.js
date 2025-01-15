957c67a54648543717135f364bd81954eaacb629
Arbaaz chouhan <chouhanarbaaz4@gmail.com>
2025-01-15 23:38:55 +0530

    daily-solve-dsa-questions

---
+++ JS/3-Solve-Problems-on-Arrays/question5.js
@@ -0,0 +1,27 @@
+// Left Rotate the Array by One
+// Problem Statement: Given an array of N integers, left rotate the array by one place.
+
+function LeftRotatetheArraybyOne(arr) {
+    let firstElement = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+        arr[0] = arr[arr.length - 1]
+
+    }
+
+    arr[arr.length - 1] = firstElement
+
+    return arr
+}
+
+// console.log(LeftRotatetheArraybyOne([1, 2, 3, 4, 5]));
+
+
+// 2.Example
+function LeftRotatetheArraybyOne(arr) {
+    let shift = arr.shift();
+    arr.push(shift)
+
+    return arr
+}
+
+// console.log(LeftRotatetheArraybyOne([1, 2, 3, 4, 5]));
