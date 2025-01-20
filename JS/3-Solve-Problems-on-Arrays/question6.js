ae4ecaf91b5cb66080530adc640c3bbccbc5836b
Arbaaz chouhan <chouhanarbaaz4@gmail.com>
2025-01-20 23:26:38 +0530

    Daily Solve DSA Questions

--- JS/3-Solve-Problems-on-Arrays/question6.js
+++ JS/3-Solve-Problems-on-Arrays/question6.js
@@ -4,10 +4,10 @@
 
 // // 1:Examples
 
-// function RotateArrayByKElements(arr) {
-//     let k = arr.slice(5); 
-//  let concat =  k.concat(arr.slice(0,5));
-//  return  concat
-// }
+function RotateArrayByKElements(arr) {
+    let k = arr.slice(5); 
+ let concat =  k.concat(arr.slice(0,5));
+ return  concat
+}
 
-// console.log(RotateArrayByKElements([1, 2, 3, 4, 5, 6, 7])); // output [5,6 ,7 ]
+console.log(RotateArrayByKElements([1, 2, 3, 4, 5, 6, 7])); // output [5,6 ,7 ]
