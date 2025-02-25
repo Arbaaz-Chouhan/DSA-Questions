a8af7bb5b0ffd49dd5dff8ef2fbb4d0efba1918c
Arbaaz chouhan <chouhanarbaaz4@gmail.com>
2025-02-26 01:12:36 +0530

    Daily-Solve-DSA-Questions

--- JS/3-Solve-Problems-on-Arrays/Mediume/question-3.js
+++ JS/3-Solve-Problems-on-Arrays/Mediume/question-3.js
@@ -2,25 +2,34 @@
 // Problem Statement: Given an integer array arr, find the contiguous subarray (containing at least one number) which
 // has the largest sum and returns its sum and prints the subarray.
 
+// 1.Example
+function FindMaxSubarray1(arr) {
+    return arr.reduce((acc, curr) => acc + curr)
+}
 
-function FindMaxSubarray(arr) {
-    let maxSum = arr[0];
-    let currentSum = 0; 
+console.log(FindMaxSubarray1([4, -1, 2, 1])); // 6
+console.log(FindMaxSubarray1([-2, 1, -3, 4, -1, 2, 1, -5, 4])); // 1
+
+// 2.Example
+function FindMaxSubarray2(arr) {
+    let plus = 0;
+    let less = 0;
 
     for (let i = 0; i < arr.length; i++) {
-        currentSum += arr[i]; 
 
-        if (currentSum > maxSum) {
-            maxSum = currentSum;
-        }
-
-        if (currentSum < 0) {
-            currentSum = 0;
+        if (arr[i] === +arr[i]) {
+            plus += arr[i];
+        } else if (arr[i] === -arr[i]) {
+            less += arr[i];
         }
     }
 
-    return maxSum;
+    let sum = Math.abs(plus - less)
+    // let maxSum = Math.max(plus - less)
+
+    return sum;
 }
 
-console.log(FindMaxSubarray([4, -1, 2, 1])); // 6
-console.log(FindMaxSubarray( [-2,1,-3,4,-1,2,1,-5,4] )); // 6
+console.log(FindMaxSubarray2([4, -1, 2, 1])); // 6
+console.log(FindMaxSubarray2([-2, 1, -3, 4, -1, 2, 1, -5, 4])); // 1
+
