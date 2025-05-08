ac94c92e4254008e66f7c3501d1bf46733854c23
Arbaaz chouhan <chouhanarbaaz4@gmail.com>
2025-05-08 23:13:54 +0530

    Daily-Solve-DSA-Questions

--- JS/3-Solve-Problems-on-Arrays/Hard/question-10.js
+++ JS/3-Solve-Problems-on-Arrays/Hard/question-10.js
@@ -1,19 +1,19 @@
-// Count Reverse Pairs
+// // Count Reverse Pairs
 
-function ReversePairs(arr) {
-    let count = 0;
+// function ReversePairs(arr) {
+//     let count = 0;
 
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = i + 1; j < arr.length; j++) {
-            if (arr[i] > 2 * arr[j]) {
-                count++;
-            }
-        }
-    }
+//     for (let i = 0; i < arr.length; i++) {
+//         for (let j = i + 1; j < arr.length; j++) {
+//             if (arr[i] > 2 * arr[j]) {
+//                 count++;
+//             }
+//         }
+//     }
 
-    return count;
-}
+//     return count;
+// }
 
-console.log(ReversePairs([1, 3, 2, 3, 1])); // Output: 2
-console.log(ReversePairs([3, 2, 1, 4])); // Output: 1
+// console.log(ReversePairs([1, 3, 2, 3, 1])); // Output: 2
+// console.log(ReversePairs([3, 2, 1, 4])); // Output: 1
 
