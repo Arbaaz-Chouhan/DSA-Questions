27c8deb677738e172b70102f90fea803665205ef
Arbaaz chouhan <chouhanarbaaz4@gmail.com>
2025-06-11 23:19:27 +0530

    DSA

--- JS/4-Binary-Search/question-1.js
+++ JS/4-Binary-Search/question-1.js
@@ -1,25 +1,25 @@
-function binarySearch(arr, target) {
-    let start = 0;
-    let end = arr.length - 1;
+// function binarySearch(arr, target) {
+//     let start = 0;
+//     let end = arr.length - 1;
   
-    while (start <= end) {
-      let mid = Math.floor((start + end) / 2);
+//     while (start <= end) {
+//       let mid = Math.floor((start + end) / 2);
   
-      if (arr[mid] === target) {
-        return mid;
-      } else if (arr[mid] < target) {
-        start = mid + 1;
-      } else {
-        end = mid - 1;
-      }
-    }
+//       if (arr[mid] === target) {
+//         return mid;
+//       } else if (arr[mid] < target) {
+//         start = mid + 1;
+//       } else {
+//         end = mid - 1;
+//       }
+//     }
   
-    return -1;
-  }
+//     return -1;
+//   }
   
-  // Example
-  const numbers = [1, 3, 5, 7, 9, 11];
-  const target = 9;
-  const result = binarySearch(numbers, target);
-  console.log(result); // Output: 3
+//   // Example
+//   const numbers = [1, 3, 5, 7, 9, 11];
+//   const target = 9;
+//   const result = binarySearch(numbers, target);
+//   console.log(result); // Output: 3
   
